4547eb0d7dbcd4b0a56ecea2a8a6840bfe7e30ef
karthick180887 <karthickspikey@gmail.com>
2026-05-07 11:40:42 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -17,3 +17,25 @@ def main(session: snowpark.Session):
 
     # Wrap the result in a DataFrame so the worksheet can display it as a table
     return session.create_dataframe([[ret]], schema=["RESULT"])
+==========================================================================================================
+import snowflake.snowpark as snowpark
+from snowflake.snowpark.functions import sproc
+from snowflake.snowpark.types import IntegerType
+
+def main(session: snowpark.Session):
+    session.query_tag = "func-gen"
+
+    add_two = sproc(
+        lambda session, x: session.sql(f"select {x} + 2").collect()[0][0],
+        name="add_two_proc",
+        replace=True,
+        input_types=[IntegerType()],
+        return_type=IntegerType(),
+        packages=["snowflake-snowpark-python"],
+        session=session,
+    )
+
+    ret = session.call("add_two_proc", 1)
+
+    # Return as a DataFrame so the Worksheet can render it
+    return session.create_dataframe([[ret]], schema=["ADD_TWO_RESULT"])
