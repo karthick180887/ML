3fc41681bf7c833b26d3ce1daac80c56ab1dc29e
karthick180887 <karthickspikey@gmail.com>
2026-05-07 12:45:32 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,41 +1,39 @@
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import sproc
-from snowflake.snowpark.types import IntegerType
+use schema test.public;
 
-def main(session: snowpark.Session):
-    session.query_tag = "func-gen"
+-- HANDLER required everywhere (w/ fct name / MyClass w/o process for UDTFs)
+-- stored procs always w/ Snowpark
+-- packages and import will add Snowpark
+-- no session param on UDF/UDTF with Snowpark
 
-    add_one = sproc(
-        lambda session, x: session.sql(f"select {x} + 1").collect()[0][0],
-        input_types=[IntegerType()],
-        return_type=IntegerType(),
-        packages=["snowflake-snowpark-python"],
-        session=session,
-    )
+-- =================================================================
+-- Vectorized Python UDF
+create or replace function add_v(x float, y float)
+    returns float
+    language python
+    runtime_version = 3.9
+    packages = ('pandas')
+    handler = 'add_v'
+as $$
+import pandas
+from _snowflake import vectorized
 
-    ret = add_one(1)
+@vectorized(input=pandas.DataFrame, max_batch_size=100)
+def add_v(df):
+  return df[0] + df[1]
+$$;
 
-    # Wrap the result in a DataFrame so the worksheet can display it as a table
-    return session.create_dataframe([[ret]], schema=["RESULT"])
-==========================================================================================================
-import snowflake.snowpark as snowpark
-from snowflake.snowpark.functions import sproc
-from snowflake.snowpark.types import IntegerType
+create or replace function add_2(x float, y float)
+    returns float
+    language python
+    runtime_version = 3.9
+    handler = 'add_2'
+as $$
+def add_2(x, y):
+  return x + y
+$$;
 
-def main(session: snowpark.Session):
-    session.query_tag = "func-gen"
+create or replace table xy(x float, y float);
+insert into xy values (1.0, 3.14), (2.2, 1.59), (3.0, -0.5);
 
-    add_two = sproc(
-        lambda session, x: session.sql(f"select {x} + 2").collect()[0][0],
-        name="add_two_proc",
-        replace=True,
-        input_types=[IntegerType()],
-        return_type=IntegerType(),
-        packages=["snowflake-snowpark-python"],
-        session=session,
-    )
-
-    ret = session.call("add_two_proc", 1)
-
-    # Return as a DataFrame so the Worksheet can render it
-    return session.create_dataframe([[ret]], schema=["ADD_TWO_RESULT"])
+select x, y, add_2(x, y), add_v(x, y)
+from xy;
